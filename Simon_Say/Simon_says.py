8b640fba2610b3c4abbd94c0cf0453593516d501
roshanleharwani <44237033+roshanleharwani@users.noreply.github.com>
2021-06-20 21:00:41 +0530

    Update Simon_says.py

--- Simon_Say/Simon_says.py
+++ Simon_Say/Simon_says.py
@@ -1,16 +1,22 @@
 import os 
 import random
 import time 
+import platform
 import colorama
 
 colorama.init(autoreset=True)
+oper = platform.system()
 level = int(input("Please Set Your Level [1-10]: "))
 colors = "VIBGYOR"
 simon = " "
+if oper == "Windows":
+    clear = "cls"
+else:
+    clear = "clear"
 counter = 0
 print("Get Set GO !!")
 time.sleep(2)
-os.system("cls")
+os.system(clear)
 for i in range(level):
     color = random.choice(colors)
     
@@ -22,7 +28,7 @@ for i in range(level):
     print(colorama.Style.BRIGHT + f"{color}")
     counter += 1
     time.sleep(1.5)
-    os.system("cls")
+    os.system(clear)
     
 repeat = input("Repeat: ")
 if repeat == str.strip(simon):
