40addd64140655c76db027d3a44b534202fc39f8
roshanleharwani <44237033+roshanleharwani@users.noreply.github.com>
2021-08-23 12:32:15 +0530

    Delete red-python-scripts directory

--- red-python-scripts/managed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash/
-sudo ifconfig wlan0 down
-sudo iwconfig wlan0 mode managed
-sudo ifconfig wlan0 up
-echo "Managed  Mode Enabled"
-
--- red-python-scripts/monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash/
-sudo ifconfig wlan0 down
-sudo iwconfig wlan0 mode Monitor
-sudo ifconfig wlan0 up
-echo "Monitor Mode Enabled"
